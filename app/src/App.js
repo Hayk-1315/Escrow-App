a5f5d310135b9a47135d94e7486e941d5e33ff0a
Hayk-1315 <162503366+Hayk-1315@users.noreply.github.com>
2024-12-15 21:24:06 +0100

    Add files via upload

---
+++ app/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CreateContracts from './CreateContracts';
+import ContractStorage from './ContractStorage';
+import Navbar from './Navbar';
+
+function App() {
+
+  return (
+    <Router>
+      <Navbar />
+      <div className='contract'>
+        <Routes>
+        <Route path='/' element={<CreateContracts />} />
+        <Route path='/contracts' element={<ContractStorage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ app/src/Escrow.js
@@ -0,0 +1,48 @@
+import { ethers } from 'ethers';
+import BigNumber from 'bignumber.js';
+
+
+export default function Escrow({
+  address,
+  arbiter,
+  beneficiary,
+  value,
+  handleApprove,
+}) {
+
+  // Convert the value from Wei to Ether
+  const valueInEther = ethers.utils.formatUnits(value, "ether");
+  // Format the value for better readability
+  const formattedValue = new BigNumber(valueInEther).toFormat();
+
+
+  return (
+    <div className="existing-contract">
+      <ul className="fields">
+        <li>
+          <div> Arbiter </div>
+          <div> {arbiter} </div>
+        </li>
+        <li>
+          <div> Beneficiary </div>
+          <div> {beneficiary} </div>
+        </li>
+        <li>
+          <div> Value </div>
+          <div> {formattedValue} ETH </div>
+        </li>
+        <div
+          className="button"
+          id={address}
+          onClick={(e) => {
+            e.preventDefault();
+
+            handleApprove();
+          }}
+        >
+          Approve
+        </div>
+      </ul>
+    </div>
+  );
+}
